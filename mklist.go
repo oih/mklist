2e9e1b9cfad5b6e70a4d4ba73379554b7d44d3f1
Florian Hülsmann <fh@cbix.de>
2014-09-26 12:47:43 +0200

    ajax and stuff

--- mklist.go
+++ mklist.go
@@ -1,67 +1,97 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"text/template"
+    "encoding/json"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "os/exec"
+    "sync"
+    "text/template"
 )
 
 type Group struct {
-	Name    string
-	Mail    string
-	Members []string
+    Name    string
+    Mail    string
+    Members []string
 }
 
 type List struct {
-	Date string
-	Cols [][]Group
+    Date string
+    Cols [][]Group
 }
 
+var m map[string]*sync.Mutex = make(map[string]*sync.Mutex)
+
 func main() {
-	target := "oih"
+    target := "oih"
+    // important:
+    if m[target] == nil {
+        m[target] = new(sync.Mutex)
+    }
+    http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+        if r.Method == "POST" {
+            body, err := ioutil.ReadAll(r.Body)
+            if err != nil {
+                io.WriteString(w, err.Error())
+                panic(err)
+            }
 
-	// JSON-Datei einlesen
-	jsonfile, err := ioutil.ReadFile(target + ".json")
-	if err != nil {
-		panic(err)
-	}
+            // JSON parsen
+            list := new(List)
+            err = json.Unmarshal(body, &list)
+            if err != nil {
+                io.WriteString(w, err.Error())
+                panic(err)
+            }
 
-	// JSON parsen
-	var test List
-	err = json.Unmarshal(jsonfile, &test)
-	if err != nil {
-		panic(err)
-	}
+            m[target].Lock()
+            defer m[target].Unlock()
+            err = ioutil.WriteFile(target + ".json", body, 0644)
+            if err != nil {
+                io.WriteString(w, err.Error())
+                panic(err)
+            }
+            list.Create(target)
+            io.WriteString(w, "Liste kann jetzt heruntergeladen werden!")
+        }
+    })
+    http.Handle("/", http.FileServer(http.Dir(".")))
+    err := http.ListenAndServe(":10005", nil)
+    if err != nil {
+        panic(err)
+    }
+}
 
-	// Template parsen
-	tmpl, err := template.ParseFiles(target + ".tmpl")
-	if err != nil {
-		panic(err)
-	}
+func (test *List) Create(target string) {
+    // Template parsen
+    tmpl, err := template.ParseFiles(target + ".tmpl")
+    if err != nil {
+        panic(err)
+    }
 
-	// Output-Datei (svg) anlegen
-	svgfile, err := os.Create(target + ".out.svg")
-	if err != nil {
-		panic(err)
-	}
+    // Output-Datei (svg) anlegen
+    svgfile, err := os.Create(target + ".out.svg")
+    if err != nil {
+        panic(err)
+    }
 
-	// svg schreiben
-	err = tmpl.Execute(svgfile, test)
-	if err != nil {
-		panic(err)
-	}
+    // svg schreiben
+    err = tmpl.Execute(svgfile, test)
+    if err != nil {
+        panic(err)
+    }
 
-	// pdf erzeugen
-	err = exec.Command("rsvg-convert", "-f", "pdf", "-o", target+".out.pdf", target+".out.svg").Run()
-	if err != nil {
-		panic(err)
-	}
+    // pdf erzeugen
+    err = exec.Command("rsvg-convert", "-f", "pdf", "-o", target+".out.pdf", target+".out.svg").Run()
+    if err != nil {
+        panic(err)
+    }
 
-	// png erzeugen
-	err = exec.Command("rsvg-convert", "-f", "png", "-o", target+".out.png", target+".out.svg").Run()
-	if err != nil {
-		panic(err)
-	}
+    // png erzeugen
+    err = exec.Command("rsvg-convert", "-f", "png", "-o", target+".out.png", target+".out.svg").Run()
+    if err != nil {
+        panic(err)
+    }
 }
