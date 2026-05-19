7447e13d1558edfc2ca88696753de80c8cea6596
wd-4000 <preparefordeallocation@outlook.com>
2026-05-20 01:36:11 +0200

    Bust cache

--- mklist.go
+++ mklist.go
@@ -98,6 +98,9 @@ func renderIndex(w http.ResponseWriter, r *http.Request, tmpl *template.Template
 		}
 	}
 
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if err := tmpl.Execute(w, PageData{Auth: auth, Data: list}); err != nil {
 		slog.Error("template execution failed", "error", err)
